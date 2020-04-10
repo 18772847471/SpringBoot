508fdd83e564cee26df8965ad66647cbe0a0916a
黄博 <1104583639@qq.com>
2020-04-10 09:35:18 +0800

    增加配置

--- SpringBootCRUD/src/main/java/com/example/DemoApplication.java
+++ SpringBootCRUD/src/main/java/com/example/DemoApplication.java
@@ -3,12 +3,18 @@ package com.example;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.example")
 @EnableTransactionManagement(proxyTargetClass = true)
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);

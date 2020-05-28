25cfaf0f36ee6b36cf43b6b25e30ee803080b9ea
黄博 <1104583639@qq.com>
2020-05-28 10:50:00 +0800

    多数据源

---
+++ multiple/src/main/java/com/example/multiple/MultipleApplication.java
@@ -0,0 +1,13 @@
+package com.example.multiple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultipleApplication.class, args);
+    }
+
+}
---
+++ multiple/src/test/java/com/example/multiple/MultipleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multiple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

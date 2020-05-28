ddb42525fa2899fe3d9c5eb46e1ed7512a53edd7
黄博 <1104583639@qq.com>
2020-05-28 10:44:05 +0800

    测试

---
+++ mytest/src/main/java/com/example/mytest/MytestApplication.java
@@ -0,0 +1,13 @@
+package com.example.mytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MytestApplication.class, args);
+    }
+
+}
---
+++ mytest/src/main/java/com/example/mytest/business/StringTest.java
@@ -0,0 +1,8 @@
+package com.example.mytest.business;
+
+public class StringTest {
+    public  static  void main(String[] args){
+        String s1=new String();
+        System.out.println(s1+"a");
+    }
+}
